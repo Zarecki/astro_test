c7066aff687ce0694daa1a26e95d92a033428d00
Zarecki <jonzarecki@gmail.com>
2022-05-03 15:13:39 +0100

    created basic directory structure

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def fetch():
     response = requests.get('https://data.nasa.gov/resource/gvk9-iz74.json')
     data = response.text
     jsonified = json.loads(data)
-    print(len(jsonified))
     return jsonified
 
 # if __name__ == '__main__':
