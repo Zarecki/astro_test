922ca9f9da1670676f21a2b5670349ad8d9c862b
Zarecki <jonzarecki@gmail.com>
2022-05-04 14:33:46 +0100

    adds cinditional save and scheduled fetch

--- app.py
+++ app.py
@@ -1,46 +1,30 @@
-from flask import Flask, jsonify 
-import json
-
-from helpers.helper_functions import fetch
+from flask import Flask
+import schedule
+from datetime import datetime
+from helpers.helper_functions import fetch, fetch_to_db, objects_to_json
 import repositories.facility_repository as facility_repository
-from models.facility import Facility
 
 
 app = Flask(__name__)
 
-data = fetch()
+# schedules a new fetch to be called each day
+schedule.every().day.at('23:50').do(fetch_to_db)
 
-for entry in data:
-    facility = Facility(entry['center'], entry['center_search_status'], entry['facility'], entry['record_date'][:10], entry['country'], entry['contact'], entry['phone'], entry['location']['latitude'], entry['location']['longitude'], entry['location']['human_address'], entry['city'], entry['state'], entry['zipcode'], entry[':@computed_region_bigw_e76g'], entry[':@computed_region_cbhk_fwbd'], entry[':@computed_region_nnqa_25f4'])
-
-    if('status' in entry.keys()):
-        facility.status = entry['status']
-
-    if('url_link' in entry.keys()):
-        facility.url = entry['url_link']['url']
-
-    if('occupied' in entry.keys()):
-        facility.occupied = entry['occupied'][:10]
-
-    if('last_update' in entry.keys()):
-        facility.last_update = entry['last_update'][:10]
-
-    facility_repository.save(facility)
-
-test = facility_repository.select_all()
-
+# fetching and saving to db on launch. No pun intended.
+fetch_to_db(True)
 
 @app.route('/api/', methods=['GET'])
 def main_endpoint():
-    response = []
 
-    for entry in test:
-        response.append(entry.__dict__)
-
-    return jsonify(response)
+    # retrieve data from db and return as json
+    db_data = facility_repository.select_all()
+    formatted_data = objects_to_json(db_data)
+    return formatted_data
 
 
 if __name__ == '__main__':
+    app.config['JSON_SORT_KEYS'] = False
     app.run(debug=True)
     with app.app_context():
         main_endpoint()
+        
